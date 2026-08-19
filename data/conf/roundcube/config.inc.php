6aa7dd4c8f2bcac803b941f91f76fec744b7ef44
Peter Schiffer <3899107+pschiffe@users.noreply.github.com>
2026-08-19 02:30:34 +0200

    Roundcube fixes

--- data/conf/roundcube/config.inc.php
+++ data/conf/roundcube/config.inc.php
@@ -4,6 +4,7 @@ $config['smtp_pass'] = '%p';
 $config['support_url'] = '';
 $config['product_name'] = 'Roundcube Webmail';
 $config['cipher_method'] = 'chacha20-poly1305';
+$config['session_path'] = '/rc/';
 $config['enable_installer'] = false;
 
 $config['managesieve_host'] = 'dovecot:4190';
@@ -17,6 +18,9 @@ $config['password_mailcow_api_host'] = 'https://' . getenv('MAILCOW_HOSTNAME');
 $config['password_mailcow_api_token'] = getenv('API_KEY');
 
 $roundcube_nginx_ip = gethostbyname('nginx');
+$config['proxy_whitelist'] = filter_var($roundcube_nginx_ip, FILTER_VALIDATE_IP)
+    ? [$roundcube_nginx_ip]
+    : [];
 $config['dovecot_client_ip_trusted_proxies'] = filter_var($roundcube_nginx_ip, FILTER_VALIDATE_IP)
     ? [$roundcube_nginx_ip]
     : [];
